3a3324aa6a143dba0128978010a7f29db953b916
Grigo <grigorii.taranenko@gmail.com>
2020-01-13 20:38:07 +0300

    Удалил лишние файлы

--- web/app/src/Sbis.php
+++ web/app/src/Sbis.php
@@ -7,16 +7,60 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class Sbis
 {
+    /** @var string Домаин к СБИС */
+    const URL_DOMAIN = 'https://sbis.ru';
+
     public function hello()
     {
+        var_dump($_REQUEST);
+        exit;
+        if ($_REQUEST) {
+
+        }
         $curl = new Client();
-        $response = $curl->request('GET', 'https://sbis.ru/tariffs?tab=tenders');
+        $response = $curl->request('GET', self::URL_DOMAIN . '/tariffs?tab=tenders');
         $status = $response->getStatusCode();
-        var_dump($status);
         $html = $response->getBody()->getContents();
 
         $crawler = new Crawler($html);
-        $content2 = $crawler->filter('.billing-PriceList__service-detail.billing-PriceList__tip')->last()->html();
-        echo $content2;
+
+        $crawler->filter('script')
+            ->each(function ($node) {
+                /** @var \DOMElement $domElement */
+                $domElement = $node->getNode(0);
+                $src = $domElement->getAttribute('src');
+
+                if ($src && is_string($src)) {
+                    if ($src[0] == '/') {
+                        $src = self::URL_DOMAIN . $src;
+                    }
+                    $domElement->setAttribute('src', $src);
+                    return;
+                }
+            });
+
+
+        $crawler->filter('head link')
+            ->each(function ($node) {
+                /** @var \DOMElement $domElement */
+                $domElement = $node->getNode(0);
+                $href = $domElement->getAttribute('href');
+
+                if ($href && is_string($href)) {
+                    if ($href[0] == '/') {
+                        $href = self::URL_DOMAIN . $href;
+                    }
+                    $domElement->setAttribute('href', $href);
+                    return;
+                }
+            });
+
+        $content = $crawler->html();
+        echo $content;
+        //echo $content1;
+
+        //$content = $crawler->filter('.billing-PriceList__service-detail.billing-PriceList__tip')->last()->html();
+        //echo $content;
+
     }
 }
--- web/public/index.php
+++ web/public/index.php
@@ -1,21 +1,8 @@
 <?php
 
 include '../app/vendor/autoload.php';
-use App\Acme\Foo;
 use App\Acme\Sbis;
 
-$foo = new Foo();
 $curl = new Sbis();
 
 $curl->hello();
-
-?><!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Docker <?php echo $foo->getName(); ?></title>
-    </head>
-    <body>
-        <h1>Docker <?php echo $foo->getName(); ?></h1>
-    </body>
-</html>

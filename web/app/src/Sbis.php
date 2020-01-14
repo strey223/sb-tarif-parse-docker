d82a0a51fdc63bc78f8f28621058df4dbdae1e39
Grigo <grigorii.taranenko@gmail.com>
2020-01-14 15:27:21 +0300

    Рабочая версия. Пока не понятно почему заработало

--- web/app/src/Sbis.php
+++ web/app/src/Sbis.php
@@ -16,7 +16,6 @@ class Sbis
         $response = $curl->request('GET', self::URL_DOMAIN . '/tariffs?tab=tenders');
         $status = $response->getStatusCode();
         $html = $response->getBody()->getContents();
-
         $crawler = new Crawler($html);
 
         $crawler->filter('script')
@@ -34,10 +33,20 @@ class Sbis
             });
 
 
-       /* $crawler->filter('head link')
+        /*$crawler->filter('[href]')
             ->each(function ($node) {
                 $domElement = $node->getNode(0);
                 $href = $domElement->getAttribute('href');
+
+                if (!$href || !is_string($href)) {
+                    return;
+                }
+
+                if ($href[0] !== '/') {
+                    return;
+                }
+
+                $url = self::URL_DOMAIN . $href;
 
                 if ($href && is_string($href)) {
                     if ($href[0] == '/') {
@@ -46,24 +55,63 @@ class Sbis
                     $domElement->setAttribute('href', $href);
                     return;
                 }
-            });
-*/
+            });*/
+
+      /*$crawler->filter('head link')
+        ->each(function ($node) {
+            $domElement = $node->getNode(0);
+            $href = $domElement->getAttribute('href');
+
+            if ($href && is_string($href)) {
+                if ($href[0] == '/') {
+                    $href = self::URL_DOMAIN . $href;
+                }
+                $domElement->setAttribute('href', $href);
+                return;
+            }
+        });*/
+
+
         $content = $crawler->html();
-        echo $content;
+       // echo $html;
         //echo $content1;
 
         //$content = $crawler->filter('.billing-PriceList__service-detail.billing-PriceList__tip')->last()->html();
-        //echo $content;
+        echo $content;
 
     }
 
     function redirectSbis()
     {
-        $curl = new Client();
-        $request = $_SERVER['REQUEST_URI'] ?? '';
-        $response = $curl->request('GET', self::URL_DOMAIN . $request);
-        $html = $response->getBody()->getContents();
+        $request = new Client();
+        $uri = $_SERVER['REQUEST_URI'] ?? '';
+        $requestMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 
+        $url = self::URL_DOMAIN . $uri;
+       /* $parseUrl = parse_url($url);
+        $path = $parseUrl['path'] ?? '';
+
+        $parsePathFile = pathinfo($path);
+        $extension = $parsePathFile['extension'] ?? '';*/
+
+        /*$contentType = ;
+        switch ($extension) {
+            case 'css':
+                $contentType = 'text/css';
+                break;
+            case 'js':
+                $contentType = 'text/js';
+                break;
+            case 'json':
+                $contentType = 'application/json';
+        }*/
+        $response = $request->request($requestMethod, $url, [
+     //       'headers'  => ['content-type' => $contentType],
+        ]);
+        $contentType = $response->getHeader('content-type')[0] ?? 'text/html';
+
+        $html = $response->getBody()->getContents();
+        header("Content-Type: $contentType");
         echo $html;
     }
 }
--- web/public/error.php
+++ web/public/error.php
@@ -4,4 +4,25 @@ use App\Acme\Sbis;
 $curl = new Sbis();
 $curl->redirectSbis();
 
+/* $parseUrl = parse_url($url);
+ $path = $parseUrl['path'] ?? '';
+
+ $parsePathFile = pathinfo($path);
+ $extension = $parsePathFile['extension'] ?? '';
+$contentType = 'text/html';
+       switch ($extension) {
+           case 'css':
+               $contentType = 'text/css';
+               break;
+           case 'js':
+               $contentType = 'text/js';
+               break;
+           case 'json':
+               $contentType = 'application/json';
+       }
+
+
+$handle = fopen('https://sbis.ru' . $_SERVER['REQUEST_URI'], 'rb');
+       header('Content-Type: '.$contentType);
+fpassthru($handle);*/
 exit;

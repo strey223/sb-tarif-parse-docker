1850a06561936be5a78b1e7d7ebe0b92f80f750b
streyHome <grigorii.taranenko@gmail.com>
2020-01-15 03:59:56 +0400

    hello

--- web/app/src/Sbis.php
+++ web/app/src/Sbis.php
@@ -38,7 +38,6 @@ class Sbis
             ->each(function ($node) {
                 $domElement = $node->getNode(0);
                 $href = $domElement->getAttribute('href');
-
                 if (!$href || !is_string($href)) {
                     return;
                 }
@@ -54,7 +53,7 @@ class Sbis
 
                 $query = $parseUrl['query'] ?? ''; // После ?
 
-                if ($path) {
+                if (!$path) {
                     return;
                 }
 
@@ -68,11 +67,10 @@ class Sbis
                     !$extension || !in_array($extension, ['js', 'css'])) {
                     return;
                 }
-                $assetsPath = Singleton::app()->basePath() . 'assets/';
 
-                $queryDirectory = $assetsPath;
+                $queryDirectory = Singleton::app()->basePath() . 'assets/';
                 if ($query) {
-                    $queryDirectory = $assetsPath . $query . '/';
+                    $queryDirectory .= $query . '/';
                     $isQueryDirectory = file_exists($queryDirectory);
                     if (!$isQueryDirectory) {
                         mkdir($queryDirectory);
@@ -83,15 +81,13 @@ class Sbis
 
                 if (!file_exists($pathToFile) || (time() - fileatime($pathToFile)) > self::STORAGE_CACHE_SECOND) {
 
-                    $responseHref = (new Client())->request('GET', $href);
+                    $responseHref = (new Client())->request('GET', self::URL_DOMAIN . $href);
                     $contentFile = $responseHref->getBody();
-                    file_get_contents($contentFile);
+                    file_put_contents($pathToFile, $contentFile);
                 }
             });
 
         $content = $crawler->html();
-       // echo $html;
-        //echo $content1;
 
         //$content = $crawler->filter('.billing-PriceList__service-detail.billing-PriceList__tip')->last()->html();
         header('Content-Type: text/html');
@@ -99,20 +95,84 @@ class Sbis
 
     }
 
-    function redirectSbis()
-    {
-        $time = time();
-        $request = new Client();
+
+    /**
+     * return @void
+     */
+    public function getFileOrRebirectSbis() {
         $uri = $_SERVER['REQUEST_URI'] ?? '';
         $requestMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
 
+        if ($requestMethod != 'GET') {
+            return $this->redirectSbis();
+        }
+        $parseUrl = parse_url($uri);
+
+        $path = $parseUrl['path'] ?? '';
+        $query = $parseUrl['query'] ?? ''; // После ?
+
+        if (!$path) {
+            return $this->redirectSbis();
+        }
+
+
+        $parsePathFile = pathinfo($path);
+
+        $extension = $parsePathFile['extension'] ?? ''; // php
+        $nameFile = $parsePathFile['basename'] ?? ''; // lib.inc.php
+        $pathFile = $parsePathFile['dirname'] ?? ''; // путь к файлу /www/htdocs/inc
+
+        if (!$nameFile || !$pathFile ||
+            !$extension || !in_array($extension, ['js', 'css'])) {
+            return $this->redirectSbis();
+        }
+
+        $queryDirectory = Singleton::app()->basePath() . 'assets/';
+        if ($query) {
+            $queryDirectory .= $query . '/';
+            $isQueryDirectory = file_exists($queryDirectory);
+            if (!$isQueryDirectory) {
+                return $this->redirectSbis();
+            }
+        }
+        $pathToFile = $queryDirectory . $nameFile;
+
+        if (!file_exists($pathToFile)) {
+            return $this->redirectSbis();
+        }
+        $contentType = 'text/html';
+        switch ($extension) {
+            case 'css':
+                $contentType = 'text/css';
+                break;
+            case 'js':
+                $contentType = 'text/js';
+                break;
+            case 'json':
+                $contentType = 'application/json';
+        }
+        header("Content-Type: $contentType");
+        echo  file_get_contents($pathToFile);
+    }
+
+    /**
+     * @return void
+     */
+    function redirectSbis()
+    {
+        $uri = $_SERVER['REQUEST_URI'] ?? '';
+        $requestMethod = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+
+        $request = new Client();
+
         $url = self::URL_DOMAIN . $uri;
         $response = $request->request($requestMethod, $url);
+
         $contentType = $response->getHeader('content-type')[0] ?? 'text/html';
         $html = $response->getBody()->getContents();
-        $time = time() - $time;
+
         header("Content-Type: $contentType");
-        header("X-Funny-Timer: " . $time);
+
         echo $html;
     }
 }

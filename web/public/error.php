bed5f7acd116a86f0333d0e9cb61660d580eb07d
streyHome <grigorii.taranenko@gmail.com>
2020-01-15 04:10:03 +0400

    hello

--- web/public/error.php
+++ web/public/error.php
@@ -3,21 +3,3 @@ include '../app/vendor/autoload.php';
 use App\Acme\Sbis;
 $curl = new Sbis();
 $curl->getFileOrRebirectSbis();
-
-/* $parseUrl = parse_url($url);
- $path = $parseUrl['path'] ?? '';
-
- $parsePathFile = pathinfo($path);
- $extension = $parsePathFile['extension'] ?? '';*/
-
-/*$contentType = ;
-switch ($extension) {
-    case 'css':
-        $contentType = 'text/css';
-        break;
-    case 'js':
-        $contentType = 'text/js';
-        break;
-    case 'json':
-        $contentType = 'application/json';
-}*/

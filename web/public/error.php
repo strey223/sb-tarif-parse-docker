9efa4e16e850b657b31a54398043fd276e7a8ff0
streyHome <grigorii.taranenko@gmail.com>
2020-04-10 16:09:09 +0300

    exception

--- web/public/error.php
+++ web/public/error.php
@@ -6,6 +6,7 @@ throw new Exception('Не найдена страница', 403);
 
 use App\Acme\Singleton;
 use App\Acme\Sbis;
+throw new Exception('Не найдена страница', 403);
 Singleton::app($config);
 $curl = new Sbis();
 $curl->getFileOrRebirectSbis();
